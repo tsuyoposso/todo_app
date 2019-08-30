3dfbd7d0ee3759763a8c35c602b0ed44dcb55315
長坂豪士 <nagasakatsuyoshi@nagasakagoushinoMacBook-Air.local>
2019-08-30 21:34:30 +0900

    first commit!!

---
+++ MyTODO/AddController.swift
@@ -0,0 +1,48 @@
+//
+//  AddControllerViewController.swift
+//  MyTODO
+//
+//  Created by 長坂豪士 on 2019/08/29.
+//  Copyright © 2019 Tsuyoshi Nagasaka. All rights reserved.
+//
+
+import UIKit
+
+//変数の設置
+var todoKobetsuNoNakami = [String]()
+
+class AddController: UIViewController {
+
+    //テキストフィールドの設定
+    @IBOutlet weak var TodoTextField: UITextField!
+    
+    //追加ボタンのアクション
+    @IBAction func TodoAddButton(_ sender: Any) {
+        //テキストフィールドの入力値を配列に追加
+        todoKobetsuNoNakami.append(TodoTextField.text!)
+        //テキストフィールドをクリア
+        TodoTextField.text = ""
+        //変数の中身をUDに追加する
+        //UD(UserDefaults) -> データベースを使うまでもないものを格納できる
+        //forKey: -> 格納先の名前
+        UserDefaults.standard.set(todoKobetsuNoNakami, forKey: "TodoList")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyTODO/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  MyTODO
+//
+//  Created by 長坂豪士 on 2019/08/29.
+//  Copyright © 2019 Tsuyoshi Nagasaka. All rights reserved.
+//
+
+import UIKit
+
+//クラスの継承
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    //表示するセル数を決める(UITableView, numberOfInSectionの追加)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //戻り値(表示するcell数)
+        return todoKobetsuNoNakami.count
+    }
+    
+    //表示するセルの中身を作成する(UITableView, cellForRowAtの追加)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //変数の作成 -------------------------------------------------------------------------------------↓小文字！
+        let TodoCell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+        //変数の中身を代入
+        TodoCell.textLabel!.text = todoKobetsuNoNakami[indexPath.row]
+        //戻り値
+        return TodoCell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //追加画面で入力した情報の取得
+        //TodoListに情報があったらそれを受け渡す処理
+        if UserDefaults.standard.object(forKey: "TodoList") != nil {
+            todoKobetsuNoNakami = UserDefaults.standard.object(forKey: "TodoList") as! [String]
+        }
+    }
+
+
+}
+
